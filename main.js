4c959973182a999dff617d61bcf2db406eb68b87
toyoda.akihiro <akiakiakihiro0525@icloud.com>
2022-02-09 10:54:07 +0900

    2回目提出

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
-    while(1) {
-        let returnData = prompt("日本の首都は?", "");
-        if ( returnData === null) {
-            alert("キャンセルが押されました!");   
-        } else if (returnData === "東京") {
-            alert("正解です!");
-        } else {
-            alert("不正解です!");
-        }
+while(true) {
+    let returnData = prompt('日本の首都は?');
+    if ( returnData === null) {
+        alert('キャンセルが押されました!');   
+    } else if (returnData === '東京') {
+        alert('正解です!');
+        break;　//正解したらループを抜ける。
+    } else {
+        alert('不正解です!');
     }
+}
